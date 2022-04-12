9f4aea491dc708ba136de1484677df06de2083b8
egrowing78 <egrowing78@gmail.com>
2022-04-12 23:37:23 +0900

    22-03-29 file check back-up

---
+++ FunCat_0329/FunCat/src/main/java/team4/login/controller/LoginController.java
@@ -0,0 +1,63 @@
+package team4.login.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.log4j.Log4j;
+import team4.md.domain.Member;
+import team4.md.service.LoginService;
+
+@Log4j
+@RestController
+@RequestMapping("/user")
+public class LoginController {
+	@Autowired
+	private LoginService loginService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping("login.do")
+	public ModelAndView login(Locale locale, Model model) {
+		//log.info("#LoginController login()");
+		ModelAndView mv = new ModelAndView("user/login");
+		return mv;
+	}
+	
+	@RequestMapping("signup.do")
+	public ModelAndView signup() {
+		//log.info("#LoginController signup()");
+		ModelAndView mv = new ModelAndView("user/signup");
+		return mv;
+	}
+	
+	
+	@PostMapping("login")
+	public void login(@RequestBody Member member) {
+		log.info("#LoginController login() member(id, pw): "+ member);
+//		ModelAndView mv = new ModelAndView("../");
+//		return mv;
+	}
+	
+	@PostMapping(value="signup")
+	public String signup(Member member) {
+		log.info("#LoginController signup() member: "+ member);
+		
+		loginService.signupS(member);
+//		ModelAndView mv = new ModelAndView("user/login");
+		String str = "success";
+		return str;
+	}
+	
+	
+}
---
+++ FunCat_0329/FunCat/src/main/java/team4/md/domain/Member.java
@@ -0,0 +1,29 @@
+package team4.md.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	private String member_id;
+	private String member_pw;
+	private String member_name;
+	private String member_phone;
+	private String member_email_check;
+	private Date member_rdate;
+	private String member_check;
+	
+	public Member(String member_id, String member_pw, String member_name, String member_phone, String member_email_check) {
+		super();
+		this.member_id = member_id;
+		this.member_pw = member_pw;
+		this.member_name = member_name;
+		this.member_phone = member_phone;
+		this.member_email_check = member_email_check;
+	}
+}
